36e4c7adb4da2e67c718fbc1440be7447ddb64c0
tugasSBD <ian25yola@gmail.com>
2021-04-24 04:55:31 +0700

    perubahan

--- soal2/soal2.c
+++ soal2/soal2.c
@@ -10,59 +10,71 @@
 #include <stdio.h>
 #include <wait.h>
 
-void createDir(char *basePath)
-{
-    char path[1000];
-    struct dirent *dp;
-    DIR *dir = opendir(basePath);
-    char *token;
-    char nama[612];
-    char *str;
-    if (!dir)
-        return;
-
-    while ((dp = readdir(dir)) != NULL)
-    {
-        if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0)
-        {
-            // printf("%s\n", dp->d_name);
-            str = dp->d_name;
-            snprintf(nama, sizeof nama, "%s", dp->d_name);
-            token = strtok(str, ";_");
-            int i=0;
-            while(token != NULL){
-                // printf("%s\n", token);
-                if(i==0||i==3){
-                    // printf("%s\n", token);
-                    if(fork()==0){
-                        char buf1[256];
-                        snprintf(buf1, sizeof buf1, "petshop/%s", token);
-                        char *argv[] = {"mkdir", "-p", buf1, NULL};
-                        execv("/bin/mkdir", argv);
-                    }
-                        
-                }
-                token = strtok(NULL, ";_");
-                i++;
-            }
-            strcpy(path, basePath);
-            strcat(path, "/");
-            strcat(path, dp->d_name);
-
-            createDir(path);
-        }
+void createFolder(char *nameFolder){
+    int status = 0;
+    if(fork()==0){
+        char buf1[256];
+        snprintf(buf1, sizeof buf1, "petshop/%s", nameFolder);
+        char *argv[] = {"mkdir", "-p", buf1, NULL};
+        execv("/bin/mkdir", argv);
     }
-
-    closedir(dir);
+    while(wait(&status)>0);
 }
-void copyFiles(char *basePath)
+
+void copyFiles(char *namaFolder, char *nama, char *namaFile){
+    int status = 0;
+    char buf1[256];
+    char buf2[256];
+    snprintf(buf1, sizeof buf1, "petshop/%s", namaFile);
+    snprintf(buf2, sizeof buf2, "petshop/%s/%s.jpg", namaFolder, nama);
+    printf("%s\n", buf1);
+    printf("%s\n", buf2);
+    if(fork()==0){
+        char *argv[] = {"cp", buf1, buf2, NULL};
+        execv("/bin/cp", argv);
+    }
+    while(wait(&status)>0);
+}
+
+void createKeterangan(char *folder, char *namaHewan, char *umurHewan, int pembantu, int banyak){
+    char *umur;
+    int status = 0;
+    umur = strtok(umurHewan, ".jpg");
+    char umurReal[100];
+    strcpy(umurReal, umur);
+    // printf("umurReal = %s\n", umurReal);
+    if(strcmp(umurReal, "0") == 0){
+        umur = "0.6";
+    }
+    char buf1[10000];
+    snprintf(buf1, sizeof buf1, "petshop/%s/keterangan.txt", folder);
+    printf("%s\n", buf1);
+    FILE *keterangan;
+    keterangan = fopen(buf1, "a+");
+    fprintf(keterangan, "nama : %s", namaHewan);
+    fprintf(keterangan, "\n");
+    fprintf(keterangan, "umur : %s tahun", umur);
+    fprintf(keterangan, "\n\n");
+    fclose(keterangan);
+    // printf("jumlah = %d\n", jumlah);
+    // printf("print %s umur = %s\n",umurHewan, umur);
+    
+}
+
+void deleteFiles(char *namaFiles){
+    if(fork()==0){
+        chdir("/home/ianfelix/sisop/module2/petshop");
+        char *argv[] = {"rm", namaFiles, NULL};
+        execv("/bin/rm", argv);
+    }
+}
+void checkFiles(char *basePath)
 {
     char path[1000];
     struct dirent *dp;
     DIR *dir = opendir(basePath);
     char *token;
-    char nama[612];
-    char *str;
+
     if (!dir)
         return;
 
@@ -70,43 +82,39 @@ void copyFiles(char *basePath)
     {
         if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0)
         {
-            // printf("%s\n", dp->d_name);
+            char nama[656];
+            char *str;
             str = dp->d_name;
+            // printf("%s\n", dp->d_name);
             snprintf(nama, sizeof nama, "%s", dp->d_name);
-            token = strtok(str, ";_");
-            int i=0;
-            char info[1200][1200];
-            while(token != NULL){
-                // printf("%s\n", token);
-                strcpy(info[i], token);
-                // printf("%s\n", info[i]);
-                if(i==2||i==4){
-                    // printf("%s\n", info[i]);
-                    char buf1[256];
-                    char buf2[256];
-                    if(i<3){
-                        snprintf(buf1, sizeof buf1, "petshop/%s;", nama);
-                        buf1[strlen(buf1)-1] = '\0';
-                        snprintf(buf2, sizeof buf2, "petshop/%s/%s.jpg", info[0], info[1]);
-                    }if(i>3){
-                        snprintf(buf2, sizeof buf2, "petshop/%s/%s.jpg", info[3], info[4]);
-                    }
+            token  = strtok(nama, ";_");
 
-                    if(fork()==0){
-                        char *argv[] = {"cp", buf1, buf2, NULL};
-                        execv("/bin/cp", argv);
-                    }
-                        
+            
+            char kind[1600], name[1600], age[1600];
+            char *info[2][3];
+            int banyak=0;
+            while(token != NULL){
+                int data = 0;
+                while(data<3){
+                    info[banyak][data] = token;
+                    token = strtok(NULL, ";_");
+                    data++;
                 }
-                token = strtok(NULL, ";_");
-                i++;
+                banyak++;
             }
+            char *usia;
+            for (int i = 0; i < banyak; i++) {
+                createFolder(info[i][0]);
+                copyFiles(info[i][0], info[i][1], str);
+                createKeterangan(info[i][0], info[i][1], info[i][2], i, banyak);
+            }
+            deleteFiles(str);
             // Construct new path from our base path
             strcpy(path, basePath);
             strcat(path, "/");
             strcat(path, dp->d_name);
 
-            copyFiles(path);
+            checkFiles(path);
         }
     }
 
@@ -131,12 +139,7 @@ int main()
             execv("/bin/unzip", argv);
         } else {
             while(wait(&status) > 0);
-            createDir("petshop");
-            // // this is parent
-            copyFiles("petshop");
+            checkFiles("petshop");
         }
-
-    // listFilesRecursively(path);
-
     return 0;
 }
